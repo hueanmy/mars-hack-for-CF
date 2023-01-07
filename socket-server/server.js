076642be8f9a15c7d783f95e7356ba6e9e525cf3
nmhieu28 <hieunm@creativeforce.io>
2023-01-08 01:45:52 +0700

    Merge branch 'main' of https://github.com/hueanmy/mars-hack-for-CF

--- socket-server/server.js
+++ socket-server/server.js
@@ -5,11 +5,15 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+const cors = require('cors');
+app.use(cors({ origin: '*', credentials: false }));
+
 const io = new Server(server, {
     cors: {
         origin: "*"
     }
 });
+
 const uuid = require('uuid');
 
 const users = [
